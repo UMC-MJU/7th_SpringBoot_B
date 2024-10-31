39ad865d47e37187e1852f18b13e6e7ecc54b5f4
jher235 <tim668666@gmail.com>
2024-11-01 00:27:13 +0900

    FEAT: 도메인 엔티티 작성

---
+++ 로밸-김재헌/umc-study/src/main/java/com/example/study/domain/Store.java
@@ -0,0 +1,33 @@
+package com.example.study.domain;
+
+import com.example.study.domain.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Store extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String address;
+
+    private Float score;
+
+    @OneToMany(mappedBy = "store", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Review> reviewList;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "region_id")
+    private Region region;
+
+}
---
+++ 로밸-김재헌/umc-study/src/main/java/com/example/study/domain/enums/SocialType.java
@@ -0,0 +1,5 @@
+package com.example.study.domain.enums;
+
+public enum SocialType {
+    NONE, KAKAO, GOOGLE
+}
