7fda4fe2cb427c986a50bc63b73d4062b05b1c6b
jher235 <tim668666@gmail.com>
2024-11-05 12:13:36 +0900

    FEAT: default 값 지정

--- 로밸-김재헌/umc-study/src/main/java/com/example/study/domain/Member.java
+++ 로밸-김재헌/umc-study/src/main/java/com/example/study/domain/Member.java
@@ -47,6 +47,7 @@ public class Member extends BaseEntity {
     @Column(nullable = false, length = 50)
     private String email;
 
+    @Column(nullable = false, columnDefinition = "INT DEFAULT 0")
     private Integer point;
 
     @OneToMany(mappedBy = "member", cascade = CascadeType.ALL)
