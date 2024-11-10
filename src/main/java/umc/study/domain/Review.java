c56a161e9e11c3a4aa4630d397cff71baebdda8d
eunji <tladmswl4869@naver.com>
2024-11-10 23:36:45 +0900

    6주차 실습

--- src/main/java/umc/study/domain/Review.java
+++ src/main/java/umc/study/domain/Review.java
@@ -18,10 +18,18 @@ public class Review extends BaseEntity {
     @Column(nullable = false, length = 500)
     private String title;
 
+    @Column(name = "body")
+    private String body;
+
     private Float score;
 
     // Member와의 관계를 나타내는 필드 추가
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")  // 외래 키 이름 지정
     private Member member;  // Member 필드 추가
+
+    @ManyToOne
+    @JoinColumn(name = "store_id")  // 외래 키로 사용할 컬럼 이름
+    private Store store;  // Store 엔티티와의 관계
+
 }
